0f45e7f06eb9421a64bf72c7dc5f13dd57a37705
Don <donn@tap4tools.com>
2017-11-30 13:23:19 -0900

    delete profiles

--- db/migrate/20171117183746_create_listings.rb
+++ db/migrate/20171117183746_create_listings.rb
@@ -3,8 +3,6 @@ class CreateListings < ActiveRecord::Migration [5.1]
     create_table :listings do |t|
       t.string :Name
       t.text :Description
-      t.string :Email
-      t.text :Address
       t.string :Daily_Rental_Price
       t.decimal :Weekly_Rental_Price
 
