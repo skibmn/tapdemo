fb431298d88967dab867912c5314c7511b772b13
Don <donn@tap4tools.com>
2017-12-05 12:24:18 -0900

    removed all aws stuff

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,14 +1,4 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
-  config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
-}
 end
