5527cca0c52d8e7ac1045fd48f2d11eb38e33d76
Don <donn@tap4tools.com>
2017-12-05 11:48:59 -0900

    put amazon aws infor in listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,18 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+	config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
+  end
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
