2ef47515ee90c35f4b8c0547bdfe4ee50cefe06a
Don <donn@tap4tools.com>
2017-12-07 17:18:52 -0900

    uploaders capitilization problem

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ if Rails.env.production?
   # storage :fog
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def Listing_dir
+  def listing_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
