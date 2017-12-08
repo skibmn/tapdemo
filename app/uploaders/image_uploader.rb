55a487ed2f1f84f212bf3d9c4da43b61b3d30ae9
Don <donn@tap4tools.com>
2017-12-07 16:52:16 -0900

    still working on image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ if Rails.env.production?
   # storage :fog
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def user_dir
+  def Listing_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
